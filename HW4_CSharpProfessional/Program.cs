4486c89adc0b115308a9ae1c4cf4cea683c39986
Андрей Наумов <NaumovAV1983@yandex.ru>
2023-06-02 15:00:15 +0300

    Добавьте файлы проекта.

---
+++ HW4_CSharpProfessional/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW4_CSharpProfessional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
